eacb6edafa67c751325402a6535317b0826a1472
MANU SANKAR U <115739592+zerolight59@users.noreply.github.com>
2023-09-04 21:53:37 +0530

    Add files via upload

---
+++ 9_27_simple_linklist_disp.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<malloc.h>
+struct node{
+    int data;
+    struct node *next;
+};
+typedef struct node NODE;
+
+NODE *head = NULL;
+
+void insert(int e){
+    NODE *t;
+    if(head==NULL){
+        head=(NODE *)malloc(sizeof(NODE));
+        head->data=e;
+        head->next=NULL;
+    }
+    else{
+        t=head;
+        while(t->next!=NULL){
+            t=t->next;
+        }
+        t->next=(NODE *)malloc(sizeof(NODE));
+        t->next->data=e;
+        t->next->next=NULL;
+    }
+}
+
+void disp(){
+    NODE * t;
+    if(head==NULL){
+        printf("list is empty");
+    }
+    else{
+        t=head;
+        while(t!=NULL){
+            printf("%d",t->data);
+            t=t->next;
+        }
+    }
+}
+
+int main()
+{
+   insert(10);
+   insert(20);
+   insert(30);
+   disp();
+}
